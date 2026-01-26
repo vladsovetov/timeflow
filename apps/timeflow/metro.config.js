1fd48fbcd43c16fe556a71c6b527b3c47cce2214
Vlad Sovetov <vlad.sovetov@gmail.com>
2026-01-26 10:43:15 +0200

    chore: Fixed expo build

--- apps/timeflow/metro.config.js
+++ apps/timeflow/metro.config.js
@@ -1,6 +1,19 @@
+const path = require("path");
 const { getDefaultConfig } = require("expo/metro-config");
 const { withNativeWind } = require("nativewind/metro");
 
 const config = getDefaultConfig(__dirname);
+
+// Add workspace packages to watchFolders
+const projectRoot = __dirname;
+const workspaceRoot = path.resolve(projectRoot, "../..");
+
+config.watchFolders = [workspaceRoot];
+
+// Configure resolver to handle workspace packages
+config.resolver.nodeModulesPaths = [
+  path.resolve(projectRoot, "node_modules"),
+  path.resolve(workspaceRoot, "node_modules"),
+];
 
 module.exports = withNativeWind(config, { input: "./global.css" });
